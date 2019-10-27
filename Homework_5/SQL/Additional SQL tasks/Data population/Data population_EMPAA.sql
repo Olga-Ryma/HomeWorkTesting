9d9612cd5728650c20084e99252aa0de7d444e6e
Olga-Ryma <53062542+Olga-Ryma@users.noreply.github.com>
2019-10-27 23:38:36 +0200

    Create Data population_EMPAA.sql
    Create tables with data population on sql.

---
+++ Homework_5/SQL/Additional SQL tasks/Data population/Data population_EMPAA.sql
@@ -0,0 +1,17 @@
+INSERT INTO EMPAA (EMPID, EMPNAME,  Managername, MGRID, DEPTID, SALARY, DOB) VALUES
+
+(1, 'John Smith', 'Lola', null,'10', 150.000, '1965-01-14 00:00:00'),
+
+(2, 'Mike Loomis', 'Oleg', 1,'10', 125.000, '1960-10-20 00:00:00'),
+
+3, 'Sam Martinez', 'Samanda', 2, '10', 110.000, '1965-03-08 00:00:00'),
+
+(4, 'Ann Moore', 'Kate', 2, '10', 85.000, '1996-02-26 00:00:00'),
+
+(5, 'Sam Rise', 'Oleg', 2, '20', 105.000, '1994-02-26 00:00:00'),
+
+(6, 'Nicke Smith', null, 2, '30', 165.000, '1957-02-26 00:00:00'),
+
+(7, 'Samanda Liky', 'Vova', 2, '30', 55.000, '1968-02-26 00:00:00'),
+
+(8, 'David Morts', 'Jone', 2, '30', 65.000, '1968-02-26 00:00:00');
