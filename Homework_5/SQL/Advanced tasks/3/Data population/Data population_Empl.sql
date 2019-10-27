21b2607c75312cc53e119fde00769297c7583df9
Olga-Ryma <53062542+Olga-Ryma@users.noreply.github.com>
2019-10-27 23:45:34 +0200

    Create Data population_Empl.sql

---
+++ Homework_5/SQL/Advanced tasks/3/Data population/Data population_Empl.sql
@@ -0,0 +1,7 @@
+INSERT INTO Empl ( e_id, Name, Salary) VALUES
+('10', 'Olga', '300'),
+('11', 'Nina', '500'),
+('12', 'Oleg', '800'),
+('20', 'Tanya', '1000'),
+('21', 'Artem', '1500'),
+('22', 'Dima', '1900');
