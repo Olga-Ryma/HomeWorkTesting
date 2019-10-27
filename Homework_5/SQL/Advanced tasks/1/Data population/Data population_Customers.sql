e4377aedca71b0963a638ac8b097f7c92482d80f
Olga-Ryma <53062542+Olga-Ryma@users.noreply.github.com>
2019-10-27 23:43:35 +0200

    Create Data population_Customers.sql

---
+++ Homework_5/SQL/Advanced tasks/1/Data population/Data population_Customers.sql
@@ -0,0 +1,5 @@
+INSERT INTO Customers (CustomerID, Name, Age, CountryID ) VALUES
+('1', 'Bruce Lee', '100', '5'),
+('2', 'Bruce Willis', '50', '37'),
+('3', 'Anthony Hopkins', '70', '1'),
+('4', 'Vinnie Jones', '50', '15');
